0b0d0e6dc45fd6d04c8b2411e5bf4379a92bb4ba
RathGate <mariannecorbel@hotmail.fr>
2023-02-24 16:59:00 +0100

    That dropdown was so chaotic

--- Web/4. Dropdown Menu/script.js
+++ Web/4. Dropdown Menu/script.js
@@ -1,3 +1,19 @@
 document.querySelector(".icon-menu").addEventListener("click", function() { 
     document.querySelector(".nav-left").classList.toggle("visible")
 })
+
+document.querySelectorAll(".nav-link").forEach(element => {
+    element.addEventListener("click", function() { 
+        if (window.innerWidth < 750) {
+            document.querySelector(".nav-link-container").classList.remove("visible")
+        }
+    })
+});
+
+// On switch from phone view to big screen view, removes the "visible" class on 
+// the menu (needed only when using burger menu)
+window.addEventListener("resize", function() {
+    if (this.innerWidth > 950) {
+        this.document.querySelector(".nav-link-container").classList.remove("visible")
+    } 
+})
